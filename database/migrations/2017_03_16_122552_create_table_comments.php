860b1c1d7a9f15691fd77e84b2be87f25b7e18ba
Guillaume Peres <guillaumeperes10@gmail.com>
2017-03-16 13:57:53 +0100

    Ajout de la table des commentaires

---
+++ database/migrations/2017_03_16_122552_create_table_comments.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('comments')) {
+            Schema::create('comments', function($table) {
+                $table->increments('id');
+                $table->integer('users_id');
+                $table->integer('polls_id');
+                $table->text('comment');
+                $table->timestampTz('published')->default(DB::raw('now()'));
+
+                $table->foreign('users_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('SET NULL')
+                    ->onUpdate('CASCADE');
+
+                $table->foreign('polls_id')
+                    ->references('id')
+                    ->on('polls')
+                    ->onDelete('CASCADE')
+                    ->onUpdate('CASCADE');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
