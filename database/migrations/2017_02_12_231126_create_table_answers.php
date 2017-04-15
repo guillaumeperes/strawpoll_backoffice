2274d2e9002067b8747a649f30783ea7c9699a91
Guillaume Peres <guillaumeperes10@gmail.com>
2017-04-15 21:25:05 +0200

    Close #5

--- database/migrations/2017_02_12_231126_create_table_answers.php
+++ database/migrations/2017_02_12_231126_create_table_answers.php
@@ -19,6 +19,7 @@ class CreateTableAnswers extends Migration
                 $table->increments('id');
                 $table->integer('polls_id');
                 $table->text('answer');
+                $table->integer('position')->default('0');
 
                 // Constraints
                 $table->foreign('polls_id')
